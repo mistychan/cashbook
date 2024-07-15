3717c588a481435c363b3b931e79e4a59030d2d3
W32162 <731031788@qq.com>
2024-07-15 09:07:47 +0800

    feat: 引入echarts/账单主页修改

--- src/views/home/homePage.vue
+++ src/views/home/homePage.vue
@@ -6,27 +6,75 @@
     </el-radio-group>
   </div>
   <div class="bill-container">
-    <div class="left-charts">
-      <!-- echarts图 -->
-    </div>
-    <div class="right-list">
-      <el-table :data="tableData" style="width: 100%">
-        <el-table-column
-          v-for="item in columns"
-          :key="item.id"
-          :prop="item.prop"
-          :label="item.label"
-        ></el-table-column>
-      </el-table>
-    </div>
+    <div id="expenses"></div>
+    <el-table :data="tableData">
+      <el-table-column
+        v-for="(item, index) in columns"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label"
+      ></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, onMounted } from 'vue';
+import * as echarts from 'echarts';
 const tabPosition = ref('top');
-const tableData = reactive([]);
-const columns = reactive([{}])
+const tableData = reactive([
+  { date: '2021-01-23', money: 123, type: '日常' },
+  { date: '2021-01-23', money: 123, type: '日常' },
+  { date: '2021-01-23', money: 123, type: '日常' }
+]);
+const columns = reactive([
+  { prop: 'date', label: '日期' },
+  { prop: 'money', label: '费用' },
+  { prop: 'type', label: '类别' }
+]);
+
+onMounted(() => {
+  initEcharts();
+});
+
+function initEcharts() {
+  let chart = echarts.init(document.getElementById('expenses'));
+  let option = {
+    tooltip: {
+      trigger: 'item'
+    },
+
+    series: [
+      {
+        name: '支出',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 26,
+            fontWeight: 'bold'
+          }
+        },
+        data: [
+          { value: 276, name: '购物' },
+          { value: 987, name: '餐饮' },
+          { value: 90, name: '交通' },
+          { value: 200, name: '通讯' },
+          { value: 190, name: '日常' },
+          { value: 200, name: '美妆' }
+        ]
+      }
+    ]
+  };
+  chart.setOption(option);
+}
 </script>
 
 <style lang="scss" scoped>
@@ -35,5 +83,10 @@ const columns = reactive([{}])
   justify-content: center;
   align-items: center;
   margin-top: 2rem;
+}
+#expenses {
+  margin: 0 auto;
+  width: 500px;
+  height: 500px;
 }
 </style>
